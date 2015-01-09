3fca1356e8a008c714c08e6e1a4a5080b086542c
James Holding <jamieholding10@gmail.com>
2015-01-09 21:36:42 +0000

    Example/test file updated

--- example.js
+++ example.js
@@ -10,3 +10,19 @@ api.MagicKingdom.GetWaitTimes(function(err, data) {
     
     console.log(JSON.stringify(data, null, 2));
 });
+
+api.GetURL("https://api.wdpro.disney.go.com/global-pool-override-B/facility-service/theme-parks/80007944", function(e, d) {
+   console.log(JSON.stringify(d, null, 2));
+});
+
+api.GetURL("https://api.wdpro.disney.go.com/global-pool-override-B/facility-service/destinations/80007798", function(e, d) {
+   console.log(JSON.stringify(d, null, 2));
+});
+
+api.GetPage(80010208, "Attraction", function(error, data) {
+    console.log(JSON.stringify(data, null, 2));
+});
+
+api.MagicKingdom.GetSchedule(function(err, data) {
+    console.log(JSON.stringify(data, null, 2));
+});
