a8641f837fbbcb15df6cbfe79183a7eb4ca0936b
James Holding <jamie@tehcu.be>
2015-06-14 12:40:38 +0100

    Add california parks

--- index.js
+++ index.js
@@ -5,16 +5,21 @@ var DisneyParks = require("./disneyParks");
 
 function DisneyAPI(options)
 {
-    // make our own DisneyRequest object to make API calls
+    // == make our own DisneyRequest object to make API calls ==
     var DRequest = new DisneyRequest(options);
     
-    // make park objects
+    // == make park objects ==
+    // DisneyWorld Resorts
     this.Epcot = new DisneyParks("80007838", DRequest);
     this.MagicKingdom = new DisneyParks("80007944", DRequest);
     this.HollywoodStudios = new DisneyParks("80007998", DRequest);
     this.AnimalKingdom = new DisneyParks("80007823", DRequest);
+
+    // Disneyland California
+    this.Disneyland = new DisneyParks("330339", DRequest);
+    this.CaliforniaAdventure = new DisneyParks("336894", DRequest);
     
-    // export DRequest exports too
+    // == export DRequest exports too ==
     for(var func in DRequest)
     {
         this[func] = DRequest[func];
