59906938137dca18b460861ef3a5e7b47eb9b762
Jamie Holding <me@jamie.holdings>
2015-06-19 09:03:46 +0100

    Parse the data returned from the Disney API to start to form a ride object standard if we're going to implement more ride APIs from other Disney parks

--- disneyParks.js
+++ disneyParks.js
@@ -11,15 +11,70 @@ function DisneyPark(ParkID, DRequest)
     
     
     /** Get park waiting times for rides */
-    this.GetWaitTimes = function(cb)
+    this.GetWaitTimes = function(include_entertainment)
     {
-        DRequest.GetPage(ParkID, "theme-park", "wait-times", cb);
+        // callback is the last argument sent to the function
+        var cb = arguments[ arguments.length - 1 ];
+
+        // include_entertainment defaults to false
+        if (typeof include_entertainment == "function") include_entertainment = false;
+
+        // fetch wait times from the API
+        DRequest.GetPage(ParkID, "theme-park", "wait-times", function(err, data) {
+            if (err) return cb(err);
+
+            // build ride array
+            var rides = [];
+            for(var i=0; i<data.entries.length; i++)
+            {
+                var ride = data.entries[i];
+
+                if (ride.id && ride.name && ride.type && ride.type == "Attraction")
+                {
+                    // skip non attraction rides if we've not been told to include them
+                    if (!include_entertainment)
+                    {
+                        if (ride.type != "Attraction") continue;
+                    }
+
+                    var obj = {
+                        id: ride.id,
+                        name: ride.name
+                    };
+
+                    // try to find wait time value
+                    if (ride.waitTime && ride.waitTime && ride.waitTime.postedWaitMinutes)
+                    {
+                        // report the posted wait time if present
+                        obj.waitTime = ride.waitTIme.postedWaitMinutes;
+                    }
+                    else
+                    {
+                        // zero if we cannot find a wait time
+                        obj.waitTime = 0;
+                    }
+
+                    // work out if the ride is active
+                    obj.active = (ride.waitTime && ride.waitTime.status == "Operating") ? true : false;
+
+                    // work out if we have fastpass
+                    obj.fastPass = (ride.waitTime.fastPass && ride.waitTime.fastPass.available);
+
+                    // add to our return rides array
+                    rides.push(obj);
+                }
+            }
+
+            // return rides
+            cb(null, rides);
+        });
     };
     
     /** Get park opening hours */
     this.GetSchedule = function(cb)
     {
-         DRequest.GetPage(ParkID, "schedule", cb);
+        // TODO - format this data nicely
+        DRequest.GetPage(ParkID, "schedule", cb);
     };
     
     
