d12c97c9542bb98a0e813065a88ec46c4852c7d4
Jamie Holding <me@jamie.holdings>
2015-11-25 23:52:15 +0000

    Tokyo Disneyland update - Grab localisation file from site source - Parse HTML document better to get ride wait time, status, fastpass and operating hours

--- disneyTokyo.js
+++ disneyTokyo.js
@@ -12,16 +12,20 @@ var moment = require("moment-timezone");
 var random_useragent = require('random-useragent');
 // cheerio, a jQuery-style HTML parser
 var cheerio = require('cheerio');
+// parse cookies returned by server
+var cookie = require('cookie');
+// astify to parse localisation code to get English :)
+var astify = require('astify');
+// sandbox to execute the gathered JavaScript code safely
+var jailed = require('jailed');
 
 module.exports = DisneyTokyo;
-
-var regexFindGPSCookie = /tdrloc\=([a-zA-Z0-9]{28})/g;
 
 function DisneyTokyo(options) {
 
 	// default options
 	var config = {
-		debug: false,
+		debug: true,
 		// geo-coordinates to create fake GPS positions
 		//  each park should have an array containing 2 "pair arrays"
 		//  i.e, two points to form an equater-aligned rectangle enclosing the area we're after
@@ -49,6 +53,9 @@ function DisneyTokyo(options) {
 		}
 	}
 
+	// our local localisation object
+	var loc = null;
+
 	/** Get wait times for given park_id
 	 * eg. DisneySea: tds
 	 */
@@ -58,7 +65,7 @@ function DisneyTokyo(options) {
 		// TODO - validate the park ID
 
 		// TODO - make sure we have up-to-date cookie
-		GetGeoCookie(function(err, cookie) {
+		GetGeoCookie(function(err, GPScookie) {
 			if (err) {
 				return cb(err);
 			}
@@ -70,7 +77,7 @@ function DisneyTokyo(options) {
 				headers: {
 					// TODO - make this stable! Only generate when making new cookie
 					'User-Agent': GetRandomUserAgent(),
-					'Cookie': 'tdrloc=' + cookie
+					'Cookie': 'tdrloc=' + GPScookie
 				},
 			}, function(err, resp, body) {
 				if (err) {
@@ -78,44 +85,227 @@ function DisneyTokyo(options) {
 					return cb(err);
 				}
 
-				// make a parsed HTML object of our result
-				var $ = cheerio.load(body);
-
-				// search for all ride time list objects
-				var rides = $(".schedule .midArw");
-				for (var i = 0, ride; ride = rides[i++];) {
-					var el = $(ride);
-					var ride_data = {};
-
-					// extract URL (finding ride name/id)
-					var ride_url = el.find("a")
-						.attr("href");
-					var ride_id_match = /attraction\/detail\/str_id\:([a-z0-9_]+)/gi.exec(ride_url);
-
-					// if we can't get a ride ID, just continue
-					if (!ride_id_match) {
-						console.error("Warning: " + ride_url + " doesn't supply a ride ID");
-						continue;
-					}
-
-					// got the ride ID!
-					ride_data.id = ride_id_match[1];
-
-					// get waiting time!
-					var waitTime = el.find(".waitTime");
-					if (!waitTime || !waitTime.length) {
-						ride_data.waitTime = 0;
-					} else {
-						// extract number
-						//ride_data.waitTime = ;
-					}
-
-
-
-					console.log(ride_data);
-				}
+				// parse fetched body
+				ParseTokyoHTML(body, function(err, result) {
+					return cb(err, result);
+				});
 			});
 		});
+	}
+
+	/** Parse an HTML page from Tokyo website to get ride times etc. */
+	function ParseTokyoHTML(body, cb) {
+		var results = {};
+
+		// make a parsed HTML object of our HTML body
+		var $ = cheerio.load(body);
+
+		// search for all ride time list objects
+		var rides = $(".schedule .midArw");
+		for (var i = 0, ride; ride = rides[i++];) {
+			var el = $(ride);
+			var ride_data = {};
+
+			// extract URL (finding ride name/id)
+			var ride_url = el.find("a")
+				.attr("href");
+			var ride_id_match = /attraction\/detail\/str_id\:([a-z0-9_]+)/gi.exec(ride_url);
+
+			// if we can't get a ride ID, just continue
+			if (!ride_id_match) {
+				console.error("Warning: " + ride_url + " doesn't supply a ride ID");
+				continue;
+			}
+
+			// got the ride ID!
+			ride_data.id = ride_id_match[1];
+
+			// get waiting time!
+			var waitTime = el.find(".waitTime");
+			if (!waitTime || !waitTime.length) {
+				ride_data.waitTime = 0;
+			} else {
+				// extract number
+				ride_data.waitTime = parseInt(waitTime.remove("span")
+					.text(), 10);
+				// if we didn't get a number, time is unavailable! (but ride is still open)
+				//  this usually means you have to go to the ride to get wait times, and they're not on the app
+				if (isNaN(ride_data.waitTime)) ride_data.waitTime = -1;
+			}
+
+			// does this ride have FastPass?
+			if (el.find(".fp")
+				.length) {
+				// fastpass is supported! But we can also grab the current fastpass timeslot!
+				ride_data.fastpass = true;
+
+				// extract time from HTML
+				var fastpass_time_text = el.find(".fp")
+					.text();
+				var fastpass_time_match = /\((\d{2}\:\d{2})\-(\d{2}\:\d{2})\)/g.exec(fastpass_time_text);
+				if (fastpass_time_match) {
+					// convert time
+					ride_data.fastpassStart = ParseTokyoTime(fastpass_time_match[1]);
+					ride_data.fastpassEnd = ParseTokyoTime(fastpass_time_match[2]);
+
+					ride_data.fastpassStatus = "Fastpass booking: " + fastpass_time_match[1] + " - " + fastpass_time_match[2];
+				}
+			} else if (el.find(".fp-no")
+				.length) {
+				// ride supports fastpass! but they've ran out :(
+				ride_data.fastpass = true;
+				ride_data.fastpassStatus = "All Fastpass Claimed";
+			} else {
+				ride_data.fastpass = false;
+			}
+
+			// extract opening and closing time
+			var closingTimes = el.find(".run");
+			if (closingTimes.length) {
+				for (var j = 0, time; time = closingTimes[j++];) {
+					// extract opening and closing times
+					//  annoyingly these seem to be hand-entered, as they're missing leading zeroes on time earlier than 10:00
+					var time_match = /((?:[5-9]|[12]\d)\:\d{2}).*(\d{2}\:\d{2})/g.exec($(time)
+						.text());
+					if (time_match) {
+						// parse opening and closing time
+						ride_data.openingTime = ParseTokyoTime(time_match[1]);
+						ride_data.closingTime = ParseTokyoTime(time_match[2]);
+
+						break;
+					}
+				}
+			}
+
+			// ride is active if we got an opening time!
+			// TODO - check we're currently within it's opening/closing range
+			ride_data.active = ride_data.openingTime ? true : false;
+
+			// TODO - get ride name from the loc data
+
+			results[ride_data.id] = ride_data;
+		}
+
+		return cb(null, results);
+	}
+
+	var tokyoTimeFormat = "HH:mm";
+	var tokyoTimezone = "Asia/Tokyo";
+
+	function ParseTokyoTime(time) {
+		return moment.tz(time, tokyoTimeFormat, tokyoTimezone)
+			.format(config.timeFormat);
+	}
+
+	function GetRideNames(park_id, cb) {
+		// TODO - cache this once-a-day
+
+		// first, get localisation file
+		GetLocalisationObject(function(err) {
+			// make API request to get ride list
+			request({
+				url: "http://www.tokyodisneyresort.jp/api/v1/wapi_attractions/lists/sort_type:1/locale:1/park_kind:1/",
+				headers: {
+					"Referer": "http://www.tokyodisneyresort.jp/en/attraction/lists/park:" + park_id,
+					"User-Agent": GetRandomUserAgent(),
+					"X-Requested-With": "XMLHttpRequest",
+					"Accept": "text/javascript, application/javascript, */*; q=0.01",
+				},
+				// auto-parse the result with requestjs
+				json: true
+			}, function(err, resp, body) {
+				if (err) {
+					console.log("Error getting ride names: " + err);
+					return cb(err);
+				}
+
+				//console.log(JSON.stringify(body, null, 2));
+				//console.log(JSON.stringify(loc, null, 2));
+
+				var rides = {};
+
+				if (body && body.entries && body.entries.length) {
+					for (var i = 0, ride; ride = body.entries[i++];) {
+						// calculate tags
+						var tags = [];
+						if (ride.tag_ids) {
+							for (var tag in ride.tag_ids) tags.push(GetLoc("tags", tag));
+						}
+
+						rides[ride.str_id] = {
+							id: ride.str_id,
+							name: ride.name,
+							name_yomi: ride.name_yomi,
+							image: ride.thum_url_pc,
+							// localise the tags from the website
+							tags: tags,
+							park_id: park_id,
+							// get park and area names
+							park_name: loc.place["1"][ride.park_kind],
+							area_name: GetLoc("area_name", ride.park_kind, ride.m_areas_id),
+						};
+					}
+				}
+
+				console.log(JSON.stringify(rides, null, 2));
+			});
+		});
+	}
+
+	function GetLocalisationObject(cb) {
+		// if we already have localisation data, return
+		if (loc) return cb(null);
+
+		// step 1, download the application logic holding the localisation
+		request({
+			url: "http://www.tokyodisneyresort.jp/en/js/sys-js/wapi.js",
+			method: "GET",
+			headers: {
+				'User-Agent': GetRandomUserAgent()
+			}
+		}, function(err, resp, body) {
+			if (err) {
+				console.log("Error getting localisation file: " + err);
+				return cb("Error getting localisation file");
+			}
+
+			// step 2, parse/run JS to extract the localisation data
+			var ast = astify.parse(body);
+			if (ast && ast.ast && ast.ast.body) {
+				for (var i = 0, obj; obj = ast.ast.body[i++];) {
+					// search for the _set_common localisation object
+					if (obj && obj.kind == "var" && obj.declarations && obj.declarations.length && obj.declarations[0].id && obj.declarations[0].id.name && obj.declarations[0].id.name == "_set_common") {
+						// found the localisation object! parse it!
+						var loc_src = obj.toSource() + "application.remote.send_loc(_set_common);";
+
+						// setup sandbox with callback to receive the loc object
+						var loc_sandbox = new jailed.DynamicPlugin(loc_src, {
+							send_loc: function(obj) {
+								// store returned object
+								loc = obj;
+								// disconnect sandbox
+								loc_sandbox.disconnect();
+
+								Dbg("Fetched loc strings");
+
+								return cb(null);
+							}
+						});
+					}
+				}
+			}
+		});
+	}
+
+	function GetLoc(type, id, id2) {
+		if (!loc || !loc["1"] || !loc["1"][type] || !loc["1"][type][id]) return type + ":" + id;
+
+		if (id2) {
+			if (!loc["1"][type][id][id2]) return type + ":" + id + ":" + id2;
+			return loc["1"][type][id][id2];
+		} else {
+			return loc["1"][type][id];
+		}
 	}
 
 	function GetGeoCookie(cb) {
@@ -144,23 +334,30 @@ function DisneyTokyo(options) {
 
 			// search for our GPS cookie
 			if (resp.headers && resp.headers['set-cookie'] && resp.headers['set-cookie'].length) {
-				var gpsCookie = null;
-				for (var i = 0, cookie; cookie = resp.headers['set-cookie'][i++];) {
-					// try to match each cookie against our gps cookie regex
-					var match = regexFindGPSCookie.exec(cookie);
-					if (match) {
-						// gotta match!
-						gpsCookie = match[1];
-						break;
+				var GPScookie = null;
+				for (var i = 0, cookie_string; cookie_string = resp.headers['set-cookie'][i++];) {
+					var cookie_data = cookie.parse(cookie_string);
+
+					// search for any tdrloc cookie
+					//  keep searching and keep the last set one
+					//  their server usually sets it twice, first deleting it, then setting the correct one
+					if (cookie_data && cookie_data.tdrloc) {
+						GPScookie = cookie_data.tdrloc;
 					}
 				}
 
-				if (!gpsCookie) {
+				if (!GPScookie) {
+					console.error("Failed to find valid GPS cookie");
+					for (var i = 0, cookie_string; cookie_string = resp.headers['set-cookie'][i++];) {
+						console.error(" * " + cookie_string);
+					}
 					return cb("DisneyTokyo returned no valid cookies for GPS authentication");
 				}
 
+				Dbg("Fetched GPS cookie: " + GPScookie);
+
 				// we got this far! hurrah! return the cookie value
-				return cb(null, gpsCookie);
+				return cb(null, GPScookie);
 			} else {
 				return cb("DisneyTokyo returned no cookies for GPS authentication");
 			}
@@ -190,12 +387,28 @@ function DisneyTokyo(options) {
 			return ua.userAgent.indexOf('Android') > 0;
 		});
 	}
+
+	function Dbg(msg) {
+		if (!config.debug) return;
+		console.log("[DisneyTokyo] " + msg);
+	}
+
+	this.Test = function(cb) {
+		GetRideNames("tdr", cb);
+	};
 }
 
 if (!module.parent) {
-	var app = new DisneyTokyo();
-	app.GetWaitTimes("tds", function(err, res) {
+	var cb = function(err, res) {
 		if (err) return console.error(err);
 		console.log(JSON.stringify(res, null, 2));
-	});
+	};
+
+	var app = new DisneyTokyo();
+	app.GetWaitTimes("tds", cb);
+	//app.Test(cb);
+
+	/*var file = require("fs")
+		.readFileSync(__dirname + "/test_disneysea.html");
+	app.ParseTokyoHTML(file, cb);*/
 }
