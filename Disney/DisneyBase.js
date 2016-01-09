f6b4608708cd7ba57dce57b30fa1d4dea1afa169
Jamie Holding <me@jamie.holdings>
2016-01-09 14:56:26 +0000

    Setup basic access token fetching and API requests for wdw

--- Disney/DisneyBase.js
+++ Disney/DisneyBase.js
@@ -1,24 +1,185 @@
-"use strict";
-
 // get the base Park class
 var Park = require("../parkBase");
+// request library
+var request = require("request");
 
 // export the Disney base park object
 module.exports = DisneyBase;
 
 function DisneyBase(config) {
-  // Call to parent class "Park" to inherit
-  Park.call(this, config);
+  // keep hold of 'this'
+  var self = this;
 
-  this.name = "Generic Disney Park";
+  self.name = "Generic Disney Park";
+
+  // Call to parent class "Park" to inherit
+  Park.call(self, config);
+
+  // generate a random Android useragent
+  self.RandomiseUseragent(function(ua) {
+    return (ua.osName == "Android");
+  });
+
+  // store/cache access token
+  //  will be an object with string 'token' and unix timestamp 'expires'
+  self._accessToken = null;
+  // access token URL requester
+  self._accessTokenURL = "https://authorization.go.com/token";
+  self._accessTokenURLBody = "assertion_type=public&client_id=WDPRO-MOBILE.CLIENT-PROD&grant_type=assertion";
+  self._accessTokenURLMethod = "POST";
 
   // TODO - some generic processing for Disney parks
   //  each implementation will need to define some extra logic/vars for this
   this.GetOpeningTimes = function(callback) {
     return callback("TODO - implement this");
   };
+
+  // Fetch cached/new access token for Disney API methods
+  this.GetAccessToken = function(callback) {
+    // if we already have an access token, check it is still valid
+    if (self._accessToken) {
+      // if token has expired...
+      if (self._accessToken.expires <= new Date().getTime()) {
+        // ... set to null ...
+        self.Dbg("Access token has expired, fetching new one");
+        self._accessToken = null;
+      } else {
+        // ... otherwise, return it
+        return callback(null, self._accessToken.token);
+      }
+    }
+
+    // no (valid) cached access token, request a new one!
+    self.FetchAccessToken(function(err, token, expires) {
+      if (err) return callback(err);
+
+      // cache token for later
+      self._accessToken = {
+        token: token,
+        expires: expires,
+      };
+
+      // return access token
+      return callback(null, token);
+    });
+  };
+
+  // Make the network request to create a new access token
+  this.FetchAccessToken = function(callback) {
+    // request new access token
+    request({
+        url: self._accessTokenURL,
+        method: self._accessTokenURLMethod,
+        headers: {
+          "User-Agent": self.useragent,
+        },
+        body: self._accessTokenURLBody,
+      },
+      function(err, resp, body) {
+        if (err) return self.Error("Failed to get access token", err, callback);
+
+        if (resp.statusCode != 200) {
+          return self.Error("Unexpected status code for access token response, expected 200", "Got " + resp.statusCode, callback);
+        }
+
+        // parse JSON data from response
+        var data;
+        try {
+          data = JSON.parse(body);
+        } catch (e) {
+          return self.Error("Invalid JSON returned for access token", e, callback);
+        }
+
+        if (data && data.access_token && data.expires_in) {
+          self.Dbg("Fetched access token " + data.access_token);
+
+          return callback(null, data.access_token, (new Date().getTime()) + ((data.expires_in - 30) * 1000));
+        }
+
+        return self.Error("Invalid body response for access token", null, callback);
+      }
+    );
+  };
+
+  // Fetch a WDWjs API URL
+  this.FetchURL = function(url, options, callback) {
+    // first, get a valid access token
+    self.GetAccessToken(function(err, token) {
+      if (err) return self.Error("Error getting access token for API URL", err, callback);
+
+      // setup request headers
+      var headers = {
+        'Authorization': "BEARER " + self._accessToken.token,
+        'Accept': 'application/json;apiversion=1',
+        'X-Conversation-Id': '~WDPRO-MOBILE.CLIENT-PROD',
+        "User-Agent": self.useragent,
+      };
+
+      // add/override headers if passed in
+      if (options.headers) {
+        for (var name in options.headers) headers[name] = options.headers;
+      }
+
+      // add stored load balancer instance if we have one saved
+      if (self._accessToken.correlation) {
+        headers["X-Correlation-Id"] = self._accessToken.correlation;
+      }
+
+      // setup options for request lib
+      var requestBody = {
+        url: url,
+        method: options.method || "GET",
+        headers: headers,
+      };
+
+      // add data to request object (if we have any)
+      if (options.data) {
+        if (requestBody.method == "GET") {
+          requestBody.qs = options.data;
+        } else {
+          requestBody.data = options.data;
+        }
+      }
+
+      // make request
+      request(requestBody, function(err, resp, body) {
+        // if we get an instance ID from the load balancer, store it
+        //  (even if we get an error)
+        if (resp && resp.headers && resp.headers["x-correlation-id"]) {
+          self._accessToken.correlation = resp.headers["x-correlation-id"];
+        }
+
+        if (err) return self.Error("Error making API request", err, callback);
+
+        // attempt to parse the body for JSON Data
+        var JSONData = false;
+        try {
+          JSONData = JSON.parse(body);
+        } catch (e) {
+          return self.Error("Invalid JSON data returned by API", e, callback);
+        }
+
+        return callback(null, JSONData);
+      });
+    });
+  };
 }
 
 // sort out prototype inheritance
 DisneyBase.prototype = Object.create(Park.prototype);
 DisneyBase.prototype.constructor = DisneyBase;
+
+// DEBUG TEST CODE
+if (!module.parent) {
+  // test request
+  var d = new DisneyBase();
+  // test getting Disneyland Paris schedule
+  d.FetchURL("https://api.wdpro.disney.go.com/mobile-service/public/ancestor-activities-schedules/dlp;entityType=destination", {
+    data: {
+      "filters": "theme-park,Attraction",
+      "startDate": "2016-01-09",
+      "endDate": "2016-01-29",
+      "region": "fr",
+    }
+  }, console.log);
+}
--- parkBase.js
+++ parkBase.js
@@ -1,17 +1,27 @@
-"use strict";
-
 // A basic Park object, with common shared logic for all parks
 //  All parks should extend this object
+
+// random useragent generator
+var random_useragent = require("random-useragent");
 
 // export the main object
 module.exports = Park;
 
 function Park(config) {
   // debugging is disabled by default
-  this.debug = config && config.debug || false;
+  this.debug = config && config.debug || process.env.DEBUG;
 
   // park's human-readable name
-  this.name = "Default Blank Park";
+  this.name = this.name || "Default Blank Park";
+
+  // set user-agent
+  if (config && config.useragent) {
+    // if useragent is supplied, use that one
+    this.useragent = config.useragent;
+  } else {
+    // otherwise, use a default one
+    this.useragent = this.useragent || "Mozilla/5.0 (Linux; U; Android 4.3; en-GB; C6502 Build/10.4.1.B.0.101) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30";
+  }
 
   // Get park wait times for the park
   this.GetWaitTimes = function(callback) {
@@ -28,14 +38,35 @@ function Park(config) {
   // Debug print helper function. Calls console.log with all passed arguments
   // if we're in debug mode
   this.Dbg = function() {
-    if (this.debug) console.log.apply(this, arguments);
-  }
+    if (!this.debug) return;
+    var args = ["[Debug :: " + this.name + "]", arguments.callee.caller.name];
+    for (var n in arguments) {
+      if (typeof(arguments[n] != "function")) args.push(arguments[n]);
+    }
+    console.log.apply(this, args);
+  };
+
+  // Output an error message for debugging
+  this.Error = function(message, error, callback) {
+    // print error to console nicely
+    var args = ["[ERROR :: " + this.name + "]", arguments.callee.caller.name];
+    for (var n in arguments) {
+      if (typeof(arguments[n]) != "function") args.push(arguments[n]);
+    }
+    console.error.apply(this, args);
+
+    // callback with only message part of error
+    if (callback) return callback(message);
+  };
 
   // Configure a single setting for this park
   this.Config = function(key, value) {
+    // cannot override park name
+    if (key == "name") return;
+
     // make sure this is a value we should be able to change
     if (this[key] && typeof(this[key]) != "undefined" && typeof(this[key]) != "function") {
-      this.Dbg(" * Configuring park '" + this.name + "': " + key + " = " + JSON.stringify(value));
+      this.Dbg("Configuring park '" + this.name + "': " + key + " = " + JSON.stringify(value));
       this[key] = value;
     }
   };
@@ -46,6 +77,16 @@ function Park(config) {
     for (var key in config) {
       this.Config(key, config[key]);
     }
+  };
+
+  // Randomise the useragent used by this API, with an optional filter function
+  this.RandomiseUseragent = function(filterFunction) {
+    // skip if we were manually passed a useragent string
+    if (config && config.useragent) return;
+
+    // use random-useragent library to generate
+    this.useragent = random_useragent.getRandom(filterFunction);
+    this.Dbg("Generated random user-agent for " + this.name + ": " + this.useragent);
   };
 
   // automatically call configure with the passed in config variable
