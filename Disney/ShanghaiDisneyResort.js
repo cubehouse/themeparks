f85b45edd7e209465aa3ba87ea4a768d4e45c5ed
Jamie Holding <me@jamie.holdings>
2016-01-13 22:11:45 +0000

    Add Shanghai Disney Resort to codebase ready for opening (not active yet)

---
+++ Disney/ShanghaiDisneyResort.js
@@ -0,0 +1,17 @@
+var DisneyBase = require("./DisneyBase.js");
+
+function ShanghaiDisneyResort(config) {
+  var self = this;
+
+  // park configuration
+  self.name = "Shanghai Disney Resort";
+  self.park_id = "desShanghaiDisneyland";
+  self.park_timezone = "Asia/Shanghai";
+  self.park_region = "cn";
+  self.resort_id = "shdr";
+
+  // Inherit from base Disney park
+  DisneyBase.call(self, config);
+};
+ShanghaiDisneyResort.prototype = Object.create(DisneyBase.prototype);
+ShanghaiDisneyResort.prototype.constructor = ShanghaiDisneyResort;
