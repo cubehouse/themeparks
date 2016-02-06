6095f62fb13bc1f4874b2a4f9699fe2555b67fad
Jamie Holding <me@jamie.holdings>
2016-02-06 16:06:23 +0000

    Add basic pretend support for debuglog in older versions of NodeJS

--- parkBase.js
+++ parkBase.js
@@ -6,7 +6,22 @@ var random_useragent = require("random-useragent");
 
 // use standard NodeJS debug log function
 var util = require("util");
-var debuglog = util.debuglog('wdwjs');
+var debugLibName = "wdwjs";
+var debuglog = function() {};
+
+// check util has the debuglog function creator
+if (util.debuglog) {
+  debuglog = util.debuglog(debugLibName);
+} else {
+  // if we're on an older version of NodeJS without debuglog, make our own
+  //  check our module name is set in NODE_DEBUG
+  if (new RegExp('\\b' + debugLibName + '\\b', 'i').test(process.env.NODE_DEBUG)) {
+    // just use console.error if we're on an older NodeJS version
+    debuglog = function() {
+      console.error.apply(this, [debugLibName.toUpperCase(), process.pid].concat(arguments));
+    };
+  }
+}
 
 // export the main object
 module.exports = Park;
