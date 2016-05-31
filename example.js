85a1079e21270be2afa742f87452f338ec10ac99
cubehouse <me@jamie.holdings>
2016-05-31 05:33:44 +0100

    Remove 3.x code, starting fresh for 4.x

--- example.js
+++
@@ -1,9 +0,0 @@
-var parks = require("wdwjs");
-
-var HongKongDisney = new parks.DisneylandHongKong();
-
-HongKongDisney.GetWaitTimes(function(err, times) {
-  if (err) return console.error(err);
-
-  console.log(JSON.stringify(times, null, 2));
-});
